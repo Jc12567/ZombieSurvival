f8fce4c80073250b8a1ab38db22eaa9fc881dbd2
Joseph <jc12567@ms.wasatch.edu>
2021-04-29 10:42:06 -0600

    Created a crate

---
+++ Assets/Scripts/Interactables/Crates/CrateOpen.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateOpen : Interactable
+{
+    [Header("Objects")]
+    [SerializeField]
+    private GameObject openedCrate;
+    [SerializeField]
+    private GameObject crateTop;
+    [SerializeField]
+    private GameObject item;
+
+    [Header("Positions")]
+    [SerializeField]
+    private float deltaXTop;
+    [SerializeField]
+    private float deltaYTop;
+    [SerializeField]
+    private float deltaZTop;
+
+    protected override void HandleAnimation()
+    {
+        
+    }
+
+    protected override void HandleUse()
+    {
+        if (activated)
+        {
+            Instantiate(openedCrate, transform.position, transform.rotation);
+            Instantiate(crateTop, new Vector3(transform.position.x + deltaXTop, transform.position.y + deltaYTop, transform.position.z + deltaZTop), transform.rotation);
+            Destroy(this.gameObject);
+        }
+    }
+
+    protected override void Init()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
